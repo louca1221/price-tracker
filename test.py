9062ab3ad4d08cf8fdb3908b9e2d56b8c48c19b2
louca1221 <chris_louca@hotmail.co.uk>
2025-12-29 13:22:29 +0000

    Update test.py

--- test.py
+++ test.py
@@ -62,8 +62,9 @@ price = get_price()
 change = get_change()
 
 # 3. Create the final message
-message = f"📅 Date: {now_str}\n📦 Spodumene Concentrate Index (CIF China)\n💰 Price: {price} USD/mt Change: {change}"
+message = f"📅 Date: {now_str}\n📦 Spodumene Concentrate Index (CIF China)\n💰 Price: {price} USD/mt \n Change: {change}"
 
 # 4. Send and Print
 send_msg(message)
 print(f"Script finished. Result: {price}")
+
